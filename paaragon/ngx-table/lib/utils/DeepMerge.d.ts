05236fa4d062c004db4831706851581f35cf4965
PABLO ARAGON MORENO <paragon@viewnext.com>
2019-05-29 17:25:25 +0200

    Auto-generated commit

--- paaragon/ngx-table/lib/utils/DeepMerge.d.ts
+++
@@ -1,9 +0,0 @@
-export default class DeepMerge {
-    private static isMergeableObject;
-    private static emptyTarget;
-    private static cloneIfNecessary;
-    private static defaultArrayMerge;
-    private static mergeObject;
-    static deepmerge(target: any, source: any, optionsArgument?: any): any;
-    static deepmergeAll(array: any, optionsArgument: any): any;
-}
--- paaragon/ngx-table/projects.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './lib/ngx-table.types';
-export * from './lib/ngx-table.component';
-export * from './lib/ngx-table.module';
