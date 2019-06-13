973d05fce24ec5a2ad16bcc4766607a5256a229f
PABLO ARAGON MORENO <paragon@viewnext.com>
2019-06-13 17:02:33 +0200

    fix: lint

--- projects/paaragon/ngx-table/src/lib/ngx-table.utils.ts
+++ projects/paaragon/ngx-table/src/lib/ngx-table.utils.ts
@@ -3,7 +3,8 @@ import { NgxTableConfig } from './ngx-table.types';
 export default class NgxTableUtils {
 
     public static showLastColumn(config: NgxTableConfig) {
-        return config && (config.create && config.create.enable || config.filter && config.filter.enable || config.delete && config.delete.enable);
+        return config && (config.create && config.create.enable || 
+            config.filter && config.filter.enable || config.delete && config.delete.enable);
     }
 
     public static isLockedColumn(config: NgxTableConfig, header: string) {
