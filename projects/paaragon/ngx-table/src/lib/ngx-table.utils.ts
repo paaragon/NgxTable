95c2808b1f6ec89e22fb683bf2f5758bd3155849
PABLO ARAGON MORENO <paragon@viewnext.com>
2019-06-13 17:09:56 +0200

    fix: lint

--- projects/paaragon/ngx-table/src/lib/ngx-table.utils.ts
+++ projects/paaragon/ngx-table/src/lib/ngx-table.utils.ts
@@ -3,7 +3,7 @@ import { NgxTableConfig } from './ngx-table.types';
 export default class NgxTableUtils {
 
     public static showLastColumn(config: NgxTableConfig) {
-        return config && (config.create && config.create.enable || 
+        return config && (config.create && config.create.enable ||
             config.filter && config.filter.enable || config.delete && config.delete.enable);
     }
 
